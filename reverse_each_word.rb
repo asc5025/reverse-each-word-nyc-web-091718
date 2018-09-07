9087449b19e38065e05cd975aead882e949f90fc
Albert Chun <albert.s.chun@gmail.com>
2018-09-07 21:55:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
-def reverse_each_word(sentence)
-  return string.reverse
+#def reverse_each_word(sentence)
+#  return string.reverse
 end
+
+def reverse_each_word(array)
+  nuarray = []
+  array.map! do |element|
+    element.reverse
+  end 
+array.join(" ")
+end 
